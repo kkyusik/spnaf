79a7812d9f0872c0be2761fec5c72eeb1aafac28
Q <wpkyusik@gmail.com>
2022-06-19 16:22:27 -0400

    Update SpatialWeight.R
    Edit unlisting nb.

--- R/SpatialWeight.R
+++ R/SpatialWeight.R
@@ -9,9 +9,16 @@ SpatialWeight <- function(df, shape, snap, queen){
         return(original_id[data])
     })
     names(nb) <- original_id
-
-    did <- unlist(nb)
-    oid <- substr(names(did), 1, nchar(names(did))-1)
+    
+    #--- Delete -----------
+    #did <- unlist(nb)
+    #oid <- substr(names(did), 1, nchar(names(did))-1)
+    #---------------------
+    
+    did <- stack(nb)
+    did <- setNames(did$values, did$ind)
+    oid <- names(did)
+    
     nb_frame <- data.frame(oid = oid)
     nb_frame$did <- did
     nb_frame$w <- 1
