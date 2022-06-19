809035c029843cdf685ee107d9cd2eda7e2090de
Q <wpkyusik@gmail.com>
2022-06-19 17:47:13 -0400

    Update Boot.R
    Replace n with R.

--- R/Boot.R
+++ R/Boot.R
@@ -1,13 +1,13 @@
-Boot <- function(rf, n){
+Boot <- function(rf, R){
     G <- rf$Gij
     Gset <- data.frame(no = as.numeric(row.names(rf)), G = G)
     Gset_list <- split(Gset, Gset$no)
 
     boot <- function(l){
-        sample <- c(l$G, sample(x = G[G != l$G], n-1, replace = T))
+        sample <- c(l$G, sample(x = G[G != l$G], R-1, replace = T))
         sample[is.na(sample)] <- -100
         sorted <- sort(sample, decreasing = T)
-        cr <- which(sorted == sample[1])/n
+        cr <- which(sorted == sample[1])/R
         return(cr)
     }
 
