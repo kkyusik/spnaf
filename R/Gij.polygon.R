0e4d02c11dbe0fda98ea1d123d3778671412eba7
Q <wpkyusik@gmail.com>
2022-06-19 17:41:08 -0400

    Update Gij.polygon.R
    Replace n with R to avoid confusion with n of OD.

--- R/Gij.polygon.R
+++ R/Gij.polygon.R
@@ -5,7 +5,7 @@
 #' @param queen A TRUE/FALSE input that is used to calculate \code{spdep}'s spatial contingency (Please view documents of \link[spdep]{poly2nb} for more information).
 #' @param snap A parameter that is also used to calculate \code{spdep}'s spatial contingency (Please view documents of \link[spdep]{poly2nb} for more information).
 #' @param method A string value among "o" (origin based), "d" (destination based), and "t" (both way) which determines the way to generate Spatial Weights. The default value is "t".
-#' @param n An integer value to define how many times you want to execute bootstrapping.
+#' @param R An integer value to define how many times you want to execute bootstrapping.
 #' @return The result is in the form of a list which includes a dataframe and a \code{sf} object.
 #' Both contain Gij statistics and p-value columns merged to your input df. The geometry type of the latter is linestring.
 #' @examples
@@ -26,7 +26,7 @@
 #' # Execution of Gij.polygon with data above and given parameters
 #' \donttest{
 #' result <- Gij.polygon(df = OD, shape = CA_polygon, queen = TRUE, snap = 1,
-#' method = 't', n = 1000)
+#' method = 't', R = 1000)
 #' }
 #'
 #' # check the results
@@ -46,7 +46,7 @@
 
 Gij.polygon <- function(df, shape,
                   queen = TRUE, snap = 1,
-                  method = 't', n = 1000){
+                  method = 't', R = 1000){
     oid <- did <- Gij <- NULL
 
     sw <- SpatialWeight(df, shape, snap, queen)
@@ -54,7 +54,7 @@ Gij.polygon <- function(df, shape,
     result_frame <- Gstat(SpatialWeights = sw, method = method) %>%
         dplyr::select(oid, did, n, Gij)
     # result_frame: OD data + G statistic + pval
-    result_frame <- Boot(rf = result_frame, n = n)
+    result_frame <- Boot(rf = result_frame, R = R)
     # result_lines: OD data + G statistic + pval + WKT(lines)
     result_lines <- Resultlines(shape, result_frame)
 
